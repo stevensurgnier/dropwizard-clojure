82c05c7cec1af44a22c48c79d9d05ff8149ba3ae
Steven Surgnier <ssurgnier@gmail.com>
2015-02-23 05:03:53 +0000

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dropwizard-clojure/dropwizard-clojure "0.1.0-SNAPSHOT"
+(defproject dropwizard-clojure/dropwizard-clojure "0.1.0"
   :description "DropWizard for Clojure"
   :url "https://github.com/stevensurgnier/dropwizard-clojure"
   :license {:name "Apache License 2.0"
