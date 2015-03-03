0835745520381fc374291802bdd589d1f4036f45
Steven Surgnier <ssurgnier@gmail.com>
2015-03-03 21:10:20 +0000

    Add some register-{resource, healthcheck} sugar.

--- dropwizard-clojure-core/src/main/clojure/dropwizard_clojure/core.clj
+++ dropwizard-clojure-core/src/main/clojure/dropwizard_clojure/core.clj
@@ -1,5 +1,9 @@
 (ns dropwizard-clojure.core
-  (:import [io.dropwizard Application]))
+  (:require [dropwizard-clojure.healthcheck :refer [healthcheck]])
+  (:import [io.dropwizard Application]
+           [io.dropwizard.setup Environment]
+           [io.dropwizard.jersey.setup JerseyEnvironment]
+           [com.codahale.metrics.health HealthCheckRegistry]))
 
 (defn- application
   ([app-name app-class run-fn]
@@ -18,3 +22,25 @@
 (defmacro defmain [app]
   `(def ~'-main
      (fn [& args#] (.run ^Application ~app (into-array String args#)))))
+
+(defn register-resources
+  [^Environment env resources]
+  (let [^JerseyEnvironment jersey (.jersey env)]
+    (dorun (map #(.register jersey %) resources))
+    env))
+
+(defn register-resource
+  [^Environment env resource]
+  (register-resources env [resource]))
+
+(defn register-healthchecks
+  [^Environment env healthchecks]
+  (let [^HealthCheckRegistry hc (.healthChecks env)]
+    (dorun (map (fn [[hc-name hc-fn]]
+                  (.register hc (name hc-name) (healthcheck hc-fn)))
+                healthchecks))
+    env))
+
+(defn register-healthcheck
+  [^Environment env hc-name hc-fn]
+  (register-healthchecks env {hc-name hc-fn}))
